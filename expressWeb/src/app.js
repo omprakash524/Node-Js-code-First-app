acc3daf4b5b0b68eede50e41566d2ceabdafbe69
omprakash524 <omprakashpandit524@gmail.com>
2024-02-05 12:53:48 +0530

    commit hbs template engine

--- expressWeb/src/app.js
+++ expressWeb/src/app.js
@@ -1,37 +1,61 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs = require("hbs");
 const port = process.env.PORT || 3000;
+
+
+// public static path
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates");
+const partials_path = path.join(__dirname,"../views");
+
+
+
+app.set('views',path.join(__dirname,'views'));
+// set handlebar template engine
+app.set('view engine', 'hbs');
+// app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+// app.use(express.static(partials_path));
+
+
 
 
 // app.get(route, callback)
 app.get("",(req,res)=>{
-    res.send("Welcome to om page");
+    res.render("index"); // no need to use .hbs
 });
 
 // About page
 app.get("/about",(req,res)=>{
-    res.send("Welcome to about page");
+    // now instead of send we use render bcz we used template engine handlebar
+    res.render("about");
 });
 
 // weather page
 app.get("/weather",(req,res)=>{
-    res.send("Welcome to weather page");
+    res.render("weather");
 });
 
-// Login  Page
-app.get("/login",(req,res)=>{
-    res.send("Welcome to the login page");
-});
+// // Login  Page
+// app.get("/login",(req,res)=>{
+//     res.render("Welcome to the login page");
+// });
 
 // Blog  Posts
 app.get("/blog",(req, res)=>{
-    res.send("Welcome to the blog page");
+    res.render("Welcome to the blog page");
 });
 
 
 // error page
 app.get("*",(req,res)=>{
-    res.send("This is 404 error page");
+    res.render('404err',{
+        errorMsg: 'Opps! Page Not Found'
+    });
 });
 
 
